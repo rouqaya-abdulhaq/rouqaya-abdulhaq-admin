b0242a7c5c818a6d82c0c524b06552f83d3eb51c
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-04-13 17:00:25 +0300

    created a header

---
+++ src/components/layout/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const header = (props) =>{
+    return(
+        <header>
+            <button>add blog</button>
+            <button>add project</button>
+        </header>
+    );
+}
+
+export default header;
