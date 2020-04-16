bd228d7e607ffc98a6a25dfe5aee1e701a196eb7
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-04-16 18:31:50 +0300

    created landing page

---
+++ src/components/layout/landingPage/landingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './landingPage.css';
+
+const landingPage = (props) =>{
+    return(
+        <main>
+            <h1>
+                Rouqaya Abdulhaq admin panel
+            </h1>
+        </main>
+    );
+}
+
+export default landingPage;
--- src/components/router/router.js
+++ src/components/router/router.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
+import LandingPage from '../layout/landingPage/landingPage';
 import AddBlog from '../../containers/addBlog/addBlog';
 import AddProject from '../../containers/addProject/addProject';
 
 const router = () =>{
     return(
         <Switch>
+            <Route path="/" exact component={LandingPage}/>
             <Route path="/addBlog" component={AddBlog}/>
             <Route path="/addProject" component={AddProject}/>
         </Switch>
