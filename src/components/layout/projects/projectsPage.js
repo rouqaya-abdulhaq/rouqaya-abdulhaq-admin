1f81d9593e79eb95b3b81b7b8e2f19893e930d56
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-04-25 16:26:08 +0300

    dispalyed projects in projects page

--- src/components/layout/projects/projectsPage.js
+++ src/components/layout/projects/projectsPage.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Card from '../../UI/card/card';
+
+import './projectsPage.css';
 
 const projectsPage = (props) =>{
-    console.log(props.serverData)
+
+    const projects = props.serverData ? props.serverData.map((projectData)=>{
+        return <Card title={projectData.title} info={projectData.info}
+        url={projectData.url} githubUrl={projectData.githubUrl} key={projectData.title}/>
+    }) : "no projects to display";
+    
     return(
-        <main>
-            {'props.serverData'}
+        <main className="projectsPage">
+            {projects}
         </main>
     );
 }
