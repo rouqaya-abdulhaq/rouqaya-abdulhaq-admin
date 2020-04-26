b9964574b6674da022e0a8b507d1d622dea2e5a7
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-04-26 15:42:09 +0300

    deleted blog by index with no prompt

--- src/components/layout/blogs/blogsPage.js
+++ src/components/layout/blogs/blogsPage.js
@@ -9,14 +9,30 @@ const blogs = (props) =>{
         console.log('edit project');
     }
 
-    const deleteHandler = () =>{
-        console.log('delete project');
+    const deleteHandler = (index) =>{
+        fetch('http://localhost:8000/removeBlog',{
+            method : 'DELETE',
+            headers : {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body : JSON.stringify({
+                index : index
+            })
+            }).then((res)=>{
+                return res.json();
+            }).then((res)=>{
+                props.updateState(res);
+            }).catch((err)=>{
+                console.log(err);
+            });
     }
 
-    const blogs = props.serverData ? props.serverData.map((blogData)=>{
+    const blogs = props.serverData ? props.serverData.map((blogData,index)=>{
         return <Card title={blogData.title} info={blogData.info}
         editHandler={editHandler}
         deleteHandler={deleteHandler}
+        index = {index}
         page={`/blog?blog=${blogData.title}`}  key={blogData.title}/>
     }) : "no blogs to display";
 
