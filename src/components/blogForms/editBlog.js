5b97788ad3ae54b4e2c2506b8afbef13afa0fe84
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-04-28 17:29:06 +0300

    submitted edited blog to server

--- src/components/blogForms/editBlog.js
+++ src/components/blogForms/editBlog.js
@@ -5,8 +5,6 @@ const EditBlog = (props) =>{
 
     const [blogToEdit , setBlog] = useState(null);
 
-
-
     useEffect(()=>{ 
         fetch(`http://localhost:8000/loadBlog?blogTitle=blog1`,{
             method : 'GET',
@@ -23,8 +21,32 @@ const EditBlog = (props) =>{
         });
     },[]);
 
+    const fetchEdit = (blog) =>{
+        console.log(blog);
+        fetch("http://localhost:8000/editBlog?blogTitle=blog1",{
+            method : 'PUT',
+            headers : {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body : JSON.stringify({
+                updatedBlog : {
+                    title : blog.title,
+                    content : blog.content 
+                }
+            })
+        }).then((res)=>{
+            return res.json();
+        }).then((blog)=>{
+            console.log(blog);
+            setBlog(blog);
+        }).catch((err)=>{
+            console.log(err)
+        })
+    }
+
     return(
-        <BlogForm blog={blogToEdit}/>
+        <BlogForm blog={blogToEdit} submitHandler={fetchEdit}/>
     );
 }
 
