8b205b2b164c5564fefdb69db29911c8555aa716
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-05-14 21:13:49 +0300

    uploaded img with the rest of the form

--- src/components/projectForms/addProject.js
+++ src/components/projectForms/addProject.js
@@ -4,40 +4,23 @@ import ProjectForm from '../../containers/projects/projectForm/projectForm';
 const addProject = () =>{
 
     const fetchSubmit = (project) =>{
+        const formData = new FormData();
+        formData.append('img',project.img);
+        formData.append('title',project.title);
+        formData.append('info',project.info);
+        formData.append('url',project.url);
         fetch("http://localhost:8000/addProject",{
             method : 'POST',
             headers : {
                 'Accept': 'application/json',
-                'Content-Type': 'application/json'
             },
-            body : JSON.stringify({
-                title : project.title,
-                info : project.info,
-                url : project.url,
-            })
+            body : formData
         }).then((res)=>{
             return res.json();
         }).then((project)=>{
             console.log(project);
         }).catch((err)=>{
             console.log(err)
-        })
-        uploadImg(project.img,project.title);
-    }
-
-    const uploadImg = (img,title) =>{
-        const formData = new FormData();
-        formData.append('img',img);
-        fetch(`http://localhost:8000/uploadImg?title=${title}`,{
-            method : 'POST',
-            headers : {
-                'Accept': 'application/json',
-            },
-            body : formData
-        }).then((res) =>{
-           console.log(res);
-        }).catch((err)=>{
-            console.log(err);
         })
     }
 
--- src/components/projectForms/editProject.js
+++ src/components/projectForms/editProject.js
@@ -7,47 +7,25 @@ const EditProject = (props) =>{
     
     const values = queryString.parse(props.location.search.slice(1));
     const title = values.projectTitle;
-    console.log(title);
-    async function fetchEdit  (project) {
-        uploadImg(project.img,project.title);
 
+    async function fetchEdit  (project) {
+        const formData = new FormData();
+        formData.append('img',project.img);
+        formData.append('title',project.title);
+        formData.append('info',project.info);
+        formData.append('url',project.url);
         fetch(`http://localhost:8000/editProject?projectTitle=${title}`,{
             method : 'PUT',
             headers : {
                 'Accept': 'application/json',
-                'Content-Type': 'application/json'
             },
-            body : JSON.stringify({
-                updatedProject : {
-                    title : project.title,
-                    info : project.info,
-                    url : project.url,
-                    imgUrl : 'C:\\Users\\acer\\Desktop\\projects\\rouqaya-server\\' + project.img.name
-                }
-            })
+            body : formData
         }).then((res)=>{
             return res.json();
         }).then((projects)=>{
-            // console.log(projects);
-            // props.loaction.state.updatedState("projects",projects);
+            console.log(projects);
         }).catch((err)=>{
             console.log(err)
-        })
-    }
-
-    const uploadImg = (img,title) =>{
-        const formData = new FormData();
-        formData.append('img',img);
-        fetch(`http://localhost:8000/uploadImg?title=${title}`,{
-            method : 'POST',
-            headers : {
-                'Accept': 'application/json',
-            },
-            body : formData
-        }).then((res) =>{
-           console.log(res);
-        }).catch((err)=>{
-            console.log(err);
         })
     }
 
