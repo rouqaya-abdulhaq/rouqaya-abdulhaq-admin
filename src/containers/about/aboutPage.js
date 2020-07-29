b5dfdf5eb92a79ef34ade67a7be4f0f30df3b23a
Rouqaya Abdulhaq <ruqia334@gmail.com>
2020-07-29 16:16:14 +0300

    added translation form to about

--- src/containers/about/aboutPage.js
+++ src/containers/about/aboutPage.js
@@ -5,12 +5,14 @@ class About extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            about : ""
+            about : "",
+            aboutTranslation : "",
         }
     }
 
     componentDidMount(){
         this.loadAbout();
+        this.loadAboutTranslation();
     }
 
     loadAbout = () =>{
@@ -25,6 +27,24 @@ class About extends React.Component{
             if(res.success){
                 const about = {...res.about};
                 this.setState({about : about.content});
+            }
+        }).catch((err)=>{
+            console.log(err);
+        })
+    }
+
+    loadAboutTranslation = () =>{
+        fetch('http://localhost:8000/loadAboutTranslation',{
+            method : 'GET',
+            headers : {
+                'Accept': 'application/json',
+            },
+        }).then((res)=>{
+            return res.json();
+        }).then((res)=>{
+            if(res.success){
+                const about = {...res.about};
+                this.setState({aboutTranslation : about.content});
             }
         }).catch((err)=>{
             console.log(err);
@@ -55,8 +75,36 @@ class About extends React.Component{
         })
     }
 
+    editAboutTranslation = () =>{
+        fetch('http://localhost:8000/editAboutTranslation',{
+            method : 'PUT',
+            headers : {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body : JSON.stringify({
+                data : {
+                    content : this.state.aboutTranslation
+                }
+            })
+        }).then((res)=>{
+            return res.json();
+        }).then((res)=>{
+            if(res.success){
+                const about = {...res.about};
+                this.setState({aboutTranslation : about.content});
+            }
+        }).catch((err)=>{
+            console.log(err);
+        })
+    }
+
     changeEventHandler = (event) =>{
         this.setState({about : event.target.value});
+    }
+
+    translationChangeEventHandler = (event) =>{
+        this.setState({aboutTranslation : event.target.value});
     }
 
     render(){
@@ -66,6 +114,13 @@ class About extends React.Component{
                     placeholder="content" value={this.state.about|| ''}>
                 </textarea>
                 <button onClick={this.editAbout}>
+                    Submit
+                </button>
+                <hr/>
+                <textarea dir="rtl" onChange={this.translationChangeEventHandler} 
+                    placeholder="محتوى" value={this.state.aboutTranslation|| ''}>
+                </textarea>
+                <button onClick={this.editAboutTranslation}>
                     Submit
                 </button>
             </main>
